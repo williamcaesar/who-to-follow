07b698843affad59d1455e1601f370fc2492d0e0
william <williamcesar.s.s@gmail.com>
2019-03-05 14:45:46 -0300

    break in functions

--- who_to_follow.py
+++ who_to_follow.py
@@ -2,27 +2,46 @@ import json
 import tweepy
 from textblob import TextBlob
 
-with open('credentials.json') as cred_file:
-    credentials = json.load(cred_file)
 
-auth = tweepy.OAuthHandler(credentials['consumer_key'],
-                           credentials['consumer_secret'])
+def authenticate():
+    with open('credentials.json') as cred_file:
+        credentials = json.load(cred_file)
 
-auth.set_access_token(credentials['access_token'],
-                      credentials['access_token_secret'])
+    auth = tweepy.OAuthHandler(credentials['consumer_key'],
+                               credentials['consumer_secret'])
 
-try:
-    api = tweepy.API(auth)
-except Exception as e:
-    print('We got a error authenticating')
-    print('your credentials.json are correct?')
-    print('ERROR: {}'.format(e))
+    auth.set_access_token(credentials['access_token'],
+                          credentials['access_token_secret'])
 
-subject = input('Write the subject: ')
-public_tweets = api.search(subject)
+    try:
+        api = tweepy.API(auth)
+        return api
+    except Exception as e:
+        print('We got a error authenticating')
+        print('your credentials.json are correct?')
+        print('ERROR: {}'.format(e))
 
-for tweet in public_tweets:
-    print(tweet.text)
+
+def show_tweet(tweet_obj, identation=0):
+    ident = '|{}'.format(' '*identation)
+    print('-' * 20)
+    print(ident, tweet_obj.text)
     analysis = TextBlob(tweet.text)
-    print(analysis.sentiment)
-print("")
+    print(ident, analysis.sentiment)
+    return {'sentiment': analysis.sentiment, 'retweets': tweet_obj.retweeters}
+
+
+def search():
+    api = authenticate()
+    subject = input('Write the subject: ')
+    public_tweets = api.search(subject)
+
+    print('We received {} tweets'.format(len(public_tweets)))
+
+    all = list()
+    for tweet in public_tweets:
+        all.append(show_tweet(tweet))
+
+
+if __name__ == '__main__':
+    search()
