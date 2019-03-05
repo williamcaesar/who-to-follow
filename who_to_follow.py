5692b45c1c501bb8a80b7e2ff7f2ba693cd3be89
william <williamcesar.s.s@gmail.com>
2019-03-05 17:57:42 -0300

    scrap by country

--- who_to_follow.py
+++ who_to_follow.py
@@ -28,12 +28,10 @@ def show_tweet(tweet_obj, identation=0):
     print(ident, tweet_obj.text)
     analysis = TextBlob(tweet_obj.text)
     print(ident, analysis.sentiment)
-    return {'sentiment': analysis.sentiment, 'retweets': tweet_obj.retweeters}
+    return {'sentiment': analysis.sentiment, 'text': tweet_obj.text}
 
 
-def search():
-    api = authenticate()
-    subject = input('Write the subject: ')
+def search(subject):
     public_tweets = api.search(subject)
 
     print('We received {} tweets'.format(len(public_tweets)))
@@ -41,7 +39,40 @@ def search():
     all = list()
     for tweet in public_tweets:
         all.append(show_tweet(tweet))
+    return all
+
+
+def list_trends(place):
+    place = place.capitalize()
+    trends = api.trends_available()
+    names = dict()
+    for trend in trends:
+        names[trend['name']] = trend['woeid']
+
+    if place in names.keys():
+        woeid = names[place]
+    else:
+        raise ValueError('this place does not exist in IDs')
+
+    local_trends = api.trends_place(woeid)
+    names = list()
+    for trend in local_trends:
+        for key in trend['trends']:
+            print(key)
+            names.append(key['name'])
+        print('-'*20)
+    return names
 
 
 if __name__ == '__main__':
-    search()
+    api = authenticate()
+    place = input('write the place for trend:')
+    trends = list_trends(place)
+
+    all_trend_mentions = list()
+    for trend in trends:
+        all_trend_mentions.append(search(trend))
+
+    with open('out.json', 'w') as file:
+        json.dump(all_trend_mentions, file)
+
